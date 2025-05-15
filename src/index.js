d77a880ce0540a1675cd1426b22da16976b28c18
Shad3 <122724277+LONELY-DUckWolf@users.noreply.github.com>
2025-05-15 19:21:18 +0300

    na naher

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./js/buy.js";
+import "./js/footer_modal.js";
+import "./js/mobile_modal.js";
+import "./js/reviews_modal.js";
+import "./js/scroll_down.js";
+import "./js/products_slider.js";
